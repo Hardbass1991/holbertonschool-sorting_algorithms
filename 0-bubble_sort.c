b09d74953c870da9d92eb9049093bedad6b13e0f
Hardbass1991 <midailsorridente@gmail.com>
2022-08-22 12:38:43 -0700

    controlled for null input, added function descriptions

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 #include "sort.h"
-
+/**
+ * is_sorted - tells whether an array is sorted in ascending order
+ * @array: input array
+ * @size: size of array
+ *
+ * Return: 1 if array is sorted, 0 otherwise
+ */
 int is_sorted(int *array, size_t size)
 {
 	size_t i;
 
 	for (i = 0; i < size - 1; i++)
 		if (array[i] > array[i + 1])
-			return 0;
-	return 1;
+			return (0);
+	return (1);
 }
 
+/**
+ * bubble_sort - performs a bubble sorting
+ * @array: input array
+ * @size: size of array
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, tmp;
-	
-	while (!is_sorted(array, size))
-		for (i = 0; i < size - 1; i++)
-			if (array[i] > array[i + 1])
-			{
-				tmp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = tmp;
-				print_array(array, size);
-			}
+
+	if (array)
+		while (!is_sorted(array, size))
+			for (i = 0; i < size - 1; i++)
+				if (array[i] > array[i + 1])
+				{
+					tmp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = tmp;
+					print_array(array, size);
+				}
 }
