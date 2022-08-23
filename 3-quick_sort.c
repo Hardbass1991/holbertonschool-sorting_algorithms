df4f139ee01c7fe19321e93842aa5b999583b6f1
Hardbass1991 <midailsorridente@gmail.com>
2022-08-23 10:19:43 -0700

    modified answers to questions about big O notation, and added function descriptions

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,12 +1,20 @@
 #include "sort.h"
 #include <stdio.h>
-
+/**
+ * partition - finds pivot index
+ * @array: input array
+ * @a: starting point
+ * @b: ending point
+ * @size: size of array
+ *
+ * Return: index of pivot element
+ */
 int partition(int *array, int a, int b, size_t size)
 {
 	int pivot, pivot_idx, i, tmp;
+
 	pivot = array[b];
 	pivot_idx = a - 1;
-	
 	for (i = a; i < b; i++)
 	{
 		if (array[i] <= pivot)
@@ -23,9 +31,16 @@ int partition(int *array, int a, int b, size_t size)
 	array[pivot_idx] = array[b];
 	array[b] = tmp;
 	print_array(array, size);
-	return(pivot_idx);
+	return (pivot_idx);
 }
 
+/**
+ * quick_sort_0 - performs a quick sorting
+ * @array: input array
+ * @a: starting point
+ * @b: ending point
+ * @size: size of array
+ */
 void quick_sort_0(int *array, size_t a, size_t b, size_t size)
 {
 	int p;
@@ -39,6 +54,11 @@ void quick_sort_0(int *array, size_t a, size_t b, size_t size)
 	}
 }
 
+/**
+ * quick_sort - wrapper function
+ * @array: input array
+ * @size: size of array
+ */
 void quick_sort(int *array, size_t size)
 {
 	quick_sort_0(array, 0, size, size);
